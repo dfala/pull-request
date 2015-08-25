d053cd08fb13731b03ac4817a070cb307147e336
Brack Carmony <brack.carmony@gmail.com>
2015-08-25 09:10:26 -0600

    More comments

--- magic.js
+++ magic.js
@@ -1,5 +1,7 @@
 // This part is the best part ever
-
+//Come on more comments;
+//We love comments;
+//Comments are our friends;
 var foreverRun = function () {
   console.log("Daniel Rocks");
   foreverRun();
